ff4f533c4fe370159d27ca2e49e86830570353af
Ahkrolina <ahkrolina@gmail.com>
2023-06-25 06:19:02 +0000

    Coloquei um pouco de estilo

--- src/screens/FarmaciaInfoScreen.js
+++ src/screens/FarmaciaInfoScreen.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function FarmaciaInfoScreen() {
   return (
-    <View>
-      <Text> Tecnólogo em farmácia forma profissionais capazes de planejarem e 
-		  gerenciarem o comércio, atendimento, marketing, estoque, recursos financeiros e humanos das farmácias e empresas do ramo farmacêutico.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        Tecnólogo em farmácia forma profissionais capazes de planejarem e gerenciarem o comércio, atendimento, marketing, estoque, recursos financeiros e humanos das farmácias e empresas do ramo farmacêutico.
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  text: {
+    textAlign: 'center',
+    marginHorizontal: 20,
+    color: 'darkred',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+});
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,50 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
   return (
-    <View>
-      <Button title="Sobre" onPress={() => navigation.navigate('Sobre')} />
-      <Button title="ADM" onPress={() => navigation.navigate('ADM')} />
-      <Button title="Desenvolvimento de Sistemas" onPress={() => navigation.navigate('Desenvolvimento')} />
-      <Button title="Farmácia" onPress={() => navigation.navigate('Farmacia')} />
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Sobre')}>
+        <Text style={[styles.buttonText, { fontWeight: 'bold', color: 'darkred' }]}>Sobre</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('ADM')}>
+        <Text style={[styles.buttonText, { fontWeight: 'bold', color: 'darkred' }]}>ADM</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Desenvolvimento')}>
+        <Text style={[styles.buttonText, { fontWeight: 'bold', color: 'darkred' }]}>Desenvolvimento de Sistemas</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Farmacia')}>
+        <Text style={[styles.buttonText, { fontWeight: 'bold', color: 'darkred' }]}>Farmácia</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
+  },
+  button: {
+    width: '100%',
+    height: 60,
+    marginBottom: 10,
+    backgroundColor: 'transparent',
+    borderRadius: 8,
+    borderWidth: 3,
+    borderColor: 'darkred',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
+  },
+});
+
+
+
